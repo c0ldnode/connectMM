405a226fc1b39eaab8dbd5109696a8183547ad53
C0ldnode <coldnode@protonmail.com>
2022-12-07 13:47:23 +0100

    Frontend Site

---
+++ src/App.js
@@ -0,0 +1,24 @@
+//import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/pages';
+import Info from './components/pages/Info';
+import Connect from './components/pages/Connect';
+
+
+function App() {
+  return (
+    <Router>
+    <Navbar />
+    <Routes>
+      <Route path='/' exact component={Home} />
+      <Route path='/info' component={Info} />
+      <Route path='/connect' component={Connect} />
+    </Routes>
+  </Router>
+  );
+}
+
+export default App;
---
+++ src/components/pages/Info.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Info = () => {
+    return (
+        <div
+            style={{
+                display: 'flex',
+                justifyContent: 'Right',
+                alignItems: 'Center',
+                height: '100vh'
+            }}
+        >
+            <h1>This is your wallet info</h1>
+        </div>
+    )
+}
+
+export default Info;
