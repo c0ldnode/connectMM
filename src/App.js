c642fec35f0ce8f807fd93ac0f48a0901bf8882e
C0ldnode <coldnode@protonmail.com>
2022-12-10 11:22:41 +0100

    modified:   package-lock.json 	modified:   package.json 	modified:   public/favicon.ico 	modified:   public/index.html 	deleted:    public/logo192.png 	deleted:    public/logo512.png 	modified:   src/App.js 	deleted:    src/components/Navbar/NavbarElements.js 	deleted:    src/components/Navbar/index.js 	deleted:    src/components/pages/Connect.js 	deleted:    src/components/pages/Info.js 	deleted:    src/components/pages/index.js 	modified:   src/index.js 	deleted:    src/logo.svg 	deleted:    src/setupTests.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-//import logo from './logo.svg';
-
-import React from 'react';
 import './App.css';
-import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/pages';
-import Info from './components/pages/Info';
-import Connect from './components/pages/Connect';
-
+import WalletCard from './WalletCard';
 
 function App() {
+
   return (
-    <Router>
-    <Navbar />
-    <Routes>
-      <Route path='/' exact component={Home} />
-      <Route path='/info' component={Info} />
-      <Route path='/connect' component={Connect} />
-    </Routes>
-  </Router>
+    <div className="App">
+      <WalletCard/>
+    </div>
   );
 }
 
